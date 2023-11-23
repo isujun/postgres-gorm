9ebc2dc17b36c8048af36f8e99b497206707d454
isujun <abd.ilhamrusydi@gmail.com>
2023-11-23 19:40:36 +0700

    add routes

--- main.go
+++ main.go
@@ -8,16 +8,36 @@ import (
 	"gorm.io/gorm"
 )
 
+
+type Book struct {
+	Author string `json:"author"`
+	Title string  `json:"titie"`
+	Publisher string `json:"publisher"`
+}
+
 type Repository struct {
 	DB *gorm.DB	
 }
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("/delete_book/:id", r.delete_book)
+	api.Get("/get_books", r.GetBookID)
+	api.Get("/books", r.GetBooks)
+}
+
 
 func main()  {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal(err)		
 	}	
+db, err := storage.NewConnection(config)
 
+if err != nil {
+	log.Fatal("could not load the database")
+}
 	r := Repository{
 		DB: db,
 	}

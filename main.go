2b1d50476f11605b83581f67d60a821fe835fb94
isujun <abd.ilhamrusydi@gmail.com>
2023-11-25 22:30:08 +0700

    add handler function

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"practice/postgre-gorm/models"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -18,6 +20,47 @@ type Book struct {
 type Repository struct {
 	DB *gorm.DB	
 }
+
+func (r *Repository) CreateBook(context *fiber.Ctx) error {
+	book := Book{}
+
+	err := context.BodyParser(&book)
+	
+	if err != nil {
+		context.Status(http.StatusUnprocessableEntity).JSON(
+			&fiber.Map{"message": "request failed"})
+			return err
+	}
+
+	err = r.DB.Create(&book).error
+
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message":"could not create book"})
+		return err
+	} 
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "book has been added"})
+	return nil
+}
+
+func (r *Repository) Getbook(context *fiber.Ctx) error	 {
+	bookModels := &[]models.Books{}
+	
+	err := r.DB.Find(bookModels).error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message":"could not get books"})
+			return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message":"books fetched succesfully",
+		"data":bookModels,
+	})
+	return nil
+}
+
 
 func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
